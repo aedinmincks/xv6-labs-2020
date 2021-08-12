32251f5cdfdec1ce93fa12d8fb1fac5bb2234c44
zhangzhecheng <972844572@qq.com>
2021-08-12 17:57:56 +0800

    update find

---
+++ user/find.c
@@ -0,0 +1,78 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+
+char *fmtname(char *path) {
+  char *p;
+
+  // Find first character after last slash.
+  for (p = path + strlen(path); p >= path && *p != '/'; p--)
+    ;
+  p++;
+
+  return p;
+}
+
+void find(char *path, char* filename){
+  char buf[512], *p;
+  int fd;
+  struct dirent de;
+  struct stat st;
+
+  if((fd = open(path, 0)) < 0){
+    printf("cannot open %s\r\n", path);
+    return;
+  }
+
+  if(fstat(fd, &st) < 0){
+    printf("cannot stat %s\r\n", path);
+    close(fd);
+    return;
+  }
+
+  switch (st.type) {
+  case T_FILE:
+    if(strcmp(filename, fmtname(path)) == 0){
+        printf("%s\r\n", path);
+    }
+    break;
+  case T_DIR:
+    if (strlen(path) + 1 + DIRSIZ + 1 > sizeof buf) {
+      printf("ls: path too long\n");
+      break;
+    }
+    strcpy(buf, path);
+    p = buf + strlen(buf);
+    *p++ = '/';
+    while (read(fd, &de, sizeof(de)) == sizeof(de)) {
+      if (de.inum == 0)
+        continue;
+      if (strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0)
+        continue;
+    
+      memmove(p, de.name, DIRSIZ);
+      p[DIRSIZ] = 0;
+      if (stat(buf, &st) < 0) {
+        printf("cannot stat %s\n", buf);
+        continue;
+      }
+
+      find(buf, filename);
+    }
+    break;
+  }
+  close(fd);
+}
+
+int main(int argc, char *argv[]) {
+
+  if (argc != 3) {
+    printf("argc error: %d\r\n", argc);
+    exit(1);
+  }
+
+  find(argv[1], argv[2]);
+
+  exit(0);
+}
